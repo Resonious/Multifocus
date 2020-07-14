e0af7644e660331bf1aa144fbb778559e4bc7d5a
Nigel Baillie <metreckk@gmail.com>
2020-07-14 13:35:01 +0900

    Basic design

--- src/App.svelte
+++ src/App.svelte
@@ -5,30 +5,78 @@
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  {#each tasks as task}
-    <p>{task}</p>
-  {/each}
+  <div class='tasklist'>
+    {#each tasks as task}
+      <div class='task'>
+        <button class='remove'>⮾</button>
+        <div class='taskbody'>
+          {task}
+        </div>
+      </div>
+    {/each}
+
+    <div class='task newtask'>
+      <div class='taskbody'>
+        <button class='new'>New task</button>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
     margin: 0 auto;
+
+    --boxsize: 300px;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .tasklist {
+    display: flex;
+    flex-flow: row wrap;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  .task {
+    width: var(--boxsize);
+    height: var(--boxsize);
+    text-align: left;
+
+    background-color: aliceblue;
+    border: 1px solid darkgrey;
+
+    margin: 10px;
+  }
+
+  .newtask {
+    background-color: unset;
+    border: unset;
+  }
+
+  .taskbody {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 100%;
+  }
+
+  .remove {
+    position: absolute;
+
+    border: none;
+    border-radius: 0;
+    background-color: red;
+    width: 2em;
+    height: 2em;
+  }
+
+  .new {
+    border: none;
+    border-radius: 0;
+    background-color: #9f9ff8;
+    padding: 2em;
   }
 </style>
