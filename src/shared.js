b0eec82aec7963ea108bd019923ffec19136b172
Nigel Baillie <metreckk@gmail.com>
2020-07-27 01:45:37 +0900

    Better notification content

--- src/shared.js
+++ src/shared.js
@@ -1,5 +1,9 @@
 import localForage from 'localforage';
 
+const allTags = /(<([^>]+)>)/ig;
+const lineBreaks = /<\s*((br\s*\/?)|(\/\s*div)|(\/\s*p))\s*>/ig;
+const separateOutFirstLine = /^([^\n]+)\n(.*)$/mus;
+
 // Choose weighted random index from list of weights
 function chooseIndexWeighted(chances) {
   var sum = chances.reduce((acc, el) => acc + el, 0);
@@ -11,7 +15,10 @@ function chooseIndexWeighted(chances) {
 
 // Remove HTML tags
 export function stripContent(content) {
-  return content.replace(/(<([^>]+)>)/ig, ' ');
+  return content
+    .replace(lineBreaks, "\n")
+    .replace(allTags, ' ')
+    .trim();
 }
 
 // Returns next task and mutates weights
@@ -35,8 +42,26 @@ export function pickTaskAndUpdateWeights(tasks) {
 
 // Returns Notification constructor args for the given task
 export function toNotification(task) {
-  return [
-    'Multifocus',
-    { body: stripContent(task.content) }
-  ]
+  // Start by removing tags
+  const stripped = stripContent(task.content);
+
+  // Default to just header text
+  let header = stripped;
+  let body;
+
+  // Try to break off first line and use it as header
+  if (stripped.includes("\n")) {
+    const [_, head, tail] = separateOutFirstLine.exec(stripped);
+    header = head.trim();
+    body = tail.trim();
+  }
+
+  // Don't put long content into header
+  if (header.length > 80) {
+    header = 'MFocus';
+    body = stripped;
+  }
+
+  // These are arguments to the Notification constructor!
+  return [header, { body }]
 }
